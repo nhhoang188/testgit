2c72a9d78593d45dc25c0ab295f33312a0e36736
nhhoang188@gmail.com <ghnhh18896>
2021-01-29 10:50:38 +0700

    push hoang

--- src/devtest.java
+++ src/devtest.java
@@ -1,8 +1,9 @@
 public class devtest {
     public void testgit(){
         System.out.println("test");
-        System.out.println("hoang test");
+        System.out.println("hoang test lan 1");
         System.out.println("tuananh test lần 1");
+        System.out.println("hoang test lan 2");
     }
 
     public static void main(String[] args) {
